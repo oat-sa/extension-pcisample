c4262b4495e1bc4d6c0af2e2007e54cb16ca6d10
Christophe Noël <christophe@taotesting.com>
2016-09-21 17:56:11 +0200

    fixed test code to work with the new PCI asset management system

--- views/js/pciCreator/dev/textReaderInteraction/test/test.js
+++ views/js/pciCreator/dev/textReaderInteraction/test/test.js
@@ -2,27 +2,20 @@ define([
     'jquery',
     'lodash',
     'taoQtiItem/runner/qtiItemRunner',
+    'taoQtiItem/portableElementRegistry/ciRegistry',
+    'taoQtiItem/portableElementRegistry/provider/localManifestProvider',
     'json!pciSamples/pciCreator/dev/textReaderInteraction/test/data/qti.json'
-], function ($, _, qtiItemRunner, itemData){
-
+], function ($, _, qtiItemRunner, ciRegistry, pciTestProvider, itemData){
     'use strict';
 
     var runner;
 
-    //override asset loading in order to resolve it from the runtime location
-    var strategies = [{
-        name : 'portableElementLocation',
-        handle : function handlePortableElementLocation(url){
-            if(/textReaderInteraction/.test(url.toString())){
-                return '../../../pciSamples/views/js/pciCreator/dev/' + url.toString();
-            }
-        }
-    }, {
-        name : 'default',
-        handle : function defaultStrategy(url){
-            return url.toString();
-        }
-    }];
+    //manually register the pci from its manifest
+    pciTestProvider.addManifestPath(
+        'textReaderInteraction',
+        'pciSamples/pciCreator/dev/textReaderInteraction/pciCreator.json');
+    ciRegistry.resetProviders();
+    ciRegistry.registerProvider(pciTestProvider.getModuleName());
 
     module('Text Reader Interaction');
 
@@ -38,7 +31,6 @@ define([
             .on('error', function (error){
                 $('#error-display').html(error);
             })
-            .assets(strategies)
             .init()
             .render($container);
     });
